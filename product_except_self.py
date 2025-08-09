965098d6de8b71824123cc4b0d88d656c15d2dba
ZoroOp007 <bkp825301@gmail.com>
2025-08-09 10:48:18 +0530

    product except self

--- product_except_self.py
+++ product_except_self.py
@@ -1,8 +1,34 @@
+
+
 def product_ex_self(arr):
+
+    zero_count = 0
+    zero_index = -1
+
+    product = 1
+
+    for i in range(len(arr)):
+
+        if arr[i] == 0:
+            zero_count += 1
+            zero_index = i
+        else:
+            product *= arr[i]
+
+    result = [0]*len(arr)
+
+    if zero_count == 0:
+        for i in range(len(arr)):
+            result[i] = product // arr[i]
+    elif zero_count == 1:
+        result[zero_index] = product
+
+    return result
+
 
 
 if __name__ == "__main__":
 
-    arr = [2,1,4,2,5,7,3]
+    arr = [2,1,4,2,0,7,3]
 
     print(product_ex_self(arr))
